114379e4fca18c3c41526dcb7dc7272c974a28ba
Ricardo Sousa <rwmsousa@gmail.com>
2024-10-23 14:55:49 -0300

    refactor: alter application ports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
