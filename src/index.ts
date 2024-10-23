1980ad1e58b21a2bb3fcef47d658a801b74d1483
Ricardo Sousa <rwmsousa@gmail.com>
2024-10-23 15:13:03 -0300

    refactor: alter '/' route in index app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import producerRoutes from './routes/producer.routes';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', producerRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
