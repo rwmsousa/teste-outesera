9c89996687b1b5b8dd1de432968830de2c9b8f98
Ricardo Sousa <rwmsousa@gmail.com>
2024-10-26 09:31:03 -0300

    refactor: improve producer service

--- src/services/producer.service.ts
+++ src/services/producer.service.ts
@@ -1,18 +1,47 @@
 import { getProducers } from '../repositories/producer.repository';
+import { ProducerInterval } from '../types';
 
-export interface ProducerInterval {
+interface ProducerIntervalResult {
   producer: string;
   interval: number;
   previousWin: number;
   followingWin: number;
 }
 
-const getMinMaxIntervalProducers = async (): Promise<ProducerInterval[]> => {
+const getMinMaxIntervalProducers = async (): Promise<{
+  min: ProducerIntervalResult[];
+  max: ProducerIntervalResult[];
+}> => {
   try {
     const producers = await getProducers();
-    // logic to calculate the intervals and return the producers with the highest and lowest interval between awards.
-    const result: ProducerInterval[] = [];
-    return result;
+
+    const producerIntervals: ProducerIntervalResult[] = [];
+
+    for (const producerData of producers) {
+      const { producer, years } = producerData;
+      for (let i = 1; i < years.length; i++) {
+        const interval = years[i] - years[i - 1];
+        producerIntervals.push({
+          producer,
+          interval,
+          previousWin: years[i - 1],
+          followingWin: years[i],
+        });
+      }
+    }
+
+    const sortedProducers = producerIntervals.sort(
+      (a, b) => a.interval - b.interval,
+    );
+    const minIntervalProducers = sortedProducers.filter(
+      (p) => p.interval === sortedProducers[0].interval,
+    );
+    const maxIntervalProducers = sortedProducers.filter(
+      (p) =>
+        p.interval === sortedProducers[sortedProducers.length - 1].interval,
+    );
+
+    return { min: minIntervalProducers, max: maxIntervalProducers };
   } catch (error) {
     console.error(error);
     throw error;
